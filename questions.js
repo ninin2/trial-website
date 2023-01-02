4a3c26cdb333637d046e76385d3aa64f58d39fc3
ninin2 <121827691+ninin2@users.noreply.github.com>
2023-01-02 23:10:31 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers 
+let questions = [ 
+    { 
+        numb: 1, 
+        question: "Distance between (0, 2) & (-4, -1)", 
+        answer: "-4, 1", 
+        options: [ 
+            "4, -3", 
+            "-4, 2", 
+            "3, 4", 
+            "-4, 1" 
+        ] 
+    },
+     { 
+        numb: 2, 
+        question: "What does CSS stand for?", 
+        answer: "Cascading Style Sheet", 
+        options: [ 
+            "Common Style Sheet", 
+            "Colorful Style Sheet", 
+            "Computer Style Sheet", 
+            "Cascading Style Sheet" 
+        ]
+     },
+      { 
+        numb: 3, 
+        question: "What does PHP stand for?", 
+        answer: "Hypertext Preprocessor", 
+        options: [ 
+            "Hypertext Preprocessor", 
+            "Hypertext Programming", 
+            "Hypertext Preprogramming", 
+            "Hometext Preprocessor"
+        ] 
+    },
+      { 
+        numb: 4, 
+        question: "What does SQL stand for?", 
+        answer: "Structured Query Language", 
+        options: [ 
+            "Stylish Question Language", 
+            "Stylesheet Query Language", 
+            "Statement Question Language", 
+            "Structured Query Language" 
+        ]
+    },
+      { 
+        numb: 5, 
+        question: "1",
+        
+        answer: "2", 
+        options: [ 
+            "1", 
+            "1", 
+            "1", 
+            "1" 
+        ]
+    }
+]
